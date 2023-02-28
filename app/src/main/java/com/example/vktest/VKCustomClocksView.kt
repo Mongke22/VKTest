734d291da2b38312eeed69bfcf5e6dffbc5b4a10
Mongke22 <ddd.ddd99.99@mail.ru>
2023-02-28 16:12:10 +0300

    Добавление рабочего таймера в часы

--- app/src/main/java/com/example/vktest/VKCustomClocksView.kt
+++ app/src/main/java/com/example/vktest/VKCustomClocksView.kt
@@ -2,9 +2,13 @@ package com.example.vktest
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.Paint
+import android.os.Handler
+import android.os.Looper
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
-import kotlin.properties.Delegates
+import kotlin.concurrent.thread
 
 class VKCustomClocksView(
     context: Context,
@@ -26,6 +30,12 @@ class VKCustomClocksView(
     )
     constructor(context: Context) : this(context, null)
 
+    private val timer = CurrentTimePicker()
+    private var seconds = NOT_INITIALIZED
+    private var minutes = NOT_INITIALIZED
+    private var hours = NOT_INITIALIZED
+
+    //Настройки секундной стрелки
     private var secondHandColor = NOT_INITIALIZED
         set(value){
             field = value
@@ -39,6 +49,7 @@ class VKCustomClocksView(
             field = value
         }
 
+    //Настройки минутной стрелки
     private var minuteHandColor = NOT_INITIALIZED
         set(value){
             field = value
@@ -52,6 +63,7 @@ class VKCustomClocksView(
             field = value
         }
 
+    //Настройки стрелки часов
     private var hourHandColor = NOT_INITIALIZED
         set(value){
             field = value
@@ -65,6 +77,7 @@ class VKCustomClocksView(
             field = value
         }
 
+    //Настройки делений
     private var delimiterColor = NOT_INITIALIZED
         set(value){
             field = value
@@ -74,6 +87,7 @@ class VKCustomClocksView(
             field = value
         }
 
+    //Настройки циферблата
     private var numberColor = NOT_INITIALIZED
         set(value){
             field = value
@@ -83,6 +97,7 @@ class VKCustomClocksView(
             field = value
         }
 
+    //Настройки круга часов
     private var circleBackgroundColor = NOT_INITIALIZED
         set(value){
             field = value
@@ -96,42 +111,53 @@ class VKCustomClocksView(
             field = value
         }
 
+    //Кисти для рисования
+    private var secondHandPaint = Paint()
+    private var minuteHandPaint = Paint()
+    private var hourHandPaint = Paint()
+    private var delimiterPaint = Paint()
+    private var numberPaint = Paint()
+    private var circlePaint = Paint()
+
     init {
+
         if (attributeSet != null) {
             initAttributes(attributeSet, defStyleAttr, defStyleRes)
         }else{
             initDefaultValues()
         }
+
+        turnOnClocks()
     }
 
     private fun initAttributes(attributeSet: AttributeSet, defStyleAttr: Int, defStyleRes: Int) {
         val typedArray = context.obtainStyledAttributes(
             attributeSet,
-            R.styleable.MyCustomClockVIew,
+            R.styleable.VKCustomClocksView,
             defStyleAttr,
             defStyleRes
         )
-        secondHandColor = typedArray.getColor(R.styleable.MyCustomClockVIew_secondHandColor, DEFAULT_COLOR)
-        secondHandWidth = typedArray.getInt(R.styleable.MyCustomClockVIew_secondHandWidth, DEFAULT_SECOND_HAND_WIDTH)
-        secondHandLength = typedArray.getInt(R.styleable.MyCustomClockVIew_secondHandLength, DEFAULT_SECOND_HAND_LENGTH)
+        secondHandColor = typedArray.getColor(R.styleable.VKCustomClocksView_secondHandColor, DEFAULT_COLOR)
+        secondHandWidth = typedArray.getInt(R.styleable.VKCustomClocksView_secondHandWidth, DEFAULT_SECOND_HAND_WIDTH)
+        secondHandLength = typedArray.getInt(R.styleable.VKCustomClocksView_secondHandLength, DEFAULT_SECOND_HAND_LENGTH)
 
-        minuteHandColor = typedArray.getColor(R.styleable.MyCustomClockVIew_minuteHandColor, DEFAULT_COLOR)
-        minuteHandWidth = typedArray.getInt(R.styleable.MyCustomClockVIew_minuteHandWidth, DEFAULT_MINUTE_HAND_WIDTH)
-        minuteHandLength = typedArray.getInt(R.styleable.MyCustomClockVIew_minuteHandLength, DEFAULT_MINUTE_HAND_LENGTH)
+        minuteHandColor = typedArray.getColor(R.styleable.VKCustomClocksView_minuteHandColor, DEFAULT_COLOR)
+        minuteHandWidth = typedArray.getInt(R.styleable.VKCustomClocksView_minuteHandWidth, DEFAULT_MINUTE_HAND_WIDTH)
+        minuteHandLength = typedArray.getInt(R.styleable.VKCustomClocksView_minuteHandLength, DEFAULT_MINUTE_HAND_LENGTH)
 
-        hourHandColor = typedArray.getColor(R.styleable.MyCustomClockVIew_hourHandColor, DEFAULT_COLOR)
-        hourHandWidth = typedArray.getInt(R.styleable.MyCustomClockVIew_hourHandWidth, DEFAULT_HOUR_HAND_WIDTH)
-        hourHandLength = typedArray.getInt(R.styleable.MyCustomClockVIew_hourHandLength, DEFAULT_HOUR_HAND_LENGTH)
+        hourHandColor = typedArray.getColor(R.styleable.VKCustomClocksView_hourHandColor, DEFAULT_COLOR)
+        hourHandWidth = typedArray.getInt(R.styleable.VKCustomClocksView_hourHandWidth, DEFAULT_HOUR_HAND_WIDTH)
+        hourHandLength = typedArray.getInt(R.styleable.VKCustomClocksView_hourHandLength, DEFAULT_HOUR_HAND_LENGTH)
 
-        delimiterColor = typedArray.getColor(R.styleable.MyCustomClockVIew_delimiterColor, DEFAULT_COLOR)
-        delimiterSize = typedArray.getInt(R.styleable.MyCustomClockVIew_delimiterSize, DEFAULT_DELIMITER_SIZE)
+        delimiterColor = typedArray.getColor(R.styleable.VKCustomClocksView_delimiterColor, DEFAULT_COLOR)
+        delimiterSize = typedArray.getInt(R.styleable.VKCustomClocksView_delimiterSize, DEFAULT_DELIMITER_SIZE)
 
-        numberColor = typedArray.getColor(R.styleable.MyCustomClockVIew_numberColor, DEFAULT_COLOR)
-        numberSize = typedArray.getInt(R.styleable.MyCustomClockVIew_numberSize, DEFAULT_NUMBER_SIZE)
+        numberColor = typedArray.getColor(R.styleable.VKCustomClocksView_numberColor, DEFAULT_COLOR)
+        numberSize = typedArray.getInt(R.styleable.VKCustomClocksView_numberSize, DEFAULT_NUMBER_SIZE)
 
-        circleBackgroundColor = typedArray.getColor(R.styleable.MyCustomClockVIew_circleBackgroundColor, DEFAULT_COLOR)
-        circleColor = typedArray.getColor(R.styleable.MyCustomClockVIew_circleColor, DEFAULT_COLOR)
-        circleSize = typedArray.getInt(R.styleable.MyCustomClockVIew_circleSize, DEFAULT_CIRCLE_SIZE)
+        circleBackgroundColor = typedArray.getColor(R.styleable.VKCustomClocksView_circleBackgroundColor, DEFAULT_COLOR)
+        circleColor = typedArray.getColor(R.styleable.VKCustomClocksView_circleColor, DEFAULT_COLOR)
+        circleSize = typedArray.getInt(R.styleable.VKCustomClocksView_circleSize, DEFAULT_CIRCLE_SIZE)
 
         typedArray.recycle()
     }
@@ -158,6 +184,33 @@ class VKCustomClocksView(
         circleBackgroundColor = DEFAULT_COLOR
         circleColor = DEFAULT_COLOR
         circleSize = DEFAULT_CIRCLE_SIZE
+    }
+
+    private fun turnOnClocks(){
+        thread{
+            while (true) {
+                Thread.sleep(1000)
+                Handler(Looper.getMainLooper()).post {
+                    updateTimer()
+                }
+            }
+        }
+    }
+
+    private fun updateTimer() {
+        seconds = timer.seconds
+        minutes = timer.minutes
+        hours = timer.hours
+        Log.i("Время","$hours:$minutes:$seconds")
+        /*findPositionsToDraw()
+
+        invalidate()*/
+    }
+
+    private fun findPositionsToDraw(){
+       /* findSecondHandFinishPoint()
+        findMinuteHandFinishPoint()
+        findHourHandFinishPoint()*/
     }
 
     companion object{
