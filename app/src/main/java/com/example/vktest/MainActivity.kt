f7e0881aaed04cdd3a0716ea62ccb487e29af0c8
Mongke22 <ddd.ddd99.99@mail.ru>
2023-03-01 19:33:38 +0300

    Демонстрация возможности смены настроек

--- app/src/main/java/com/example/vktest/MainActivity.kt
+++ app/src/main/java/com/example/vktest/MainActivity.kt
@@ -5,14 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SeekBar
 import com.example.vktest.databinding.ActivityMainBinding
+import com.jaredrummler.android.colorpicker.ColorPickerDialog
+import com.jaredrummler.android.colorpicker.ColorPickerDialogListener
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ColorPickerDialogListener {
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.secondColorChanger.setBackgroundColor(binding.vkTestClocks.secondHandColor)
+        binding.minuteColorChanger.setBackgroundColor(binding.vkTestClocks.minuteHandColor)
+        binding.hourColorChanger.setBackgroundColor(binding.vkTestClocks.hourHandColor)
+
         binding.secondHandLengthChanger.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 binding.vkTestClocks.secondHandLength = progress
@@ -49,6 +56,115 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+        binding.numberWidthChanger.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.vkTestClocks.numberWidth = progress
+            }
 
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+        })
+        binding.numberSizeChanger.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.vkTestClocks.numberSize = progress
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+        })
+        binding.delimiterWidthChanger.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.vkTestClocks.delimiterWidth = progress
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+        })
+        binding.circleWidthChanger.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.vkTestClocks.circleWidth = progress
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+        })
+
+        binding.secondColorChanger.setOnClickListener {
+            ColorPickerDialog.newBuilder()
+                .setDialogType(ColorPickerDialog.TYPE_CUSTOM)
+                .setAllowPresets(false)
+                .setDialogId(SECOND_COLOR_DIALOG_ID)
+                .setColor(binding.vkTestClocks.secondHandColor)
+                .setShowAlphaSlider(true)
+                .show(this);
+        }
+        binding.minuteColorChanger.setOnClickListener {
+            ColorPickerDialog.newBuilder()
+                .setDialogType(ColorPickerDialog.TYPE_CUSTOM)
+                .setAllowPresets(false)
+                .setDialogId(MINUTE_COLOR_DIALOG_ID)
+                .setColor(binding.vkTestClocks.minuteHandColor)
+                .setShowAlphaSlider(true)
+                .show(this);
+        }
+        binding.hourColorChanger.setOnClickListener {
+            ColorPickerDialog.newBuilder()
+                .setDialogType(ColorPickerDialog.TYPE_CUSTOM)
+                .setAllowPresets(false)
+                .setDialogId(HOUR_COLOR_DIALOG_ID)
+                .setColor(binding.vkTestClocks.hourHandColor)
+                .setShowAlphaSlider(true)
+                .show(this);
+        }
+
+        binding.vkTestClocks.setOnClickListener {
+            val clocks = it as VKCustomClocksView
+            if(clocks.minuteHandColor != Color.RED)
+                clocks.minuteHandColor = Color.RED
+            else clocks.minuteHandColor = Color.GREEN
+        }
+
+    }
+
+    override fun onColorSelected(dialogId: Int, color: Int) {
+        when(dialogId){
+            SECOND_COLOR_DIALOG_ID -> {
+                binding.secondColorChanger.setBackgroundColor(color)
+                binding.vkTestClocks.secondHandColor = color
+            }
+            MINUTE_COLOR_DIALOG_ID -> {
+                binding.minuteColorChanger.setBackgroundColor(color)
+                binding.vkTestClocks.minuteHandColor = color
+            }
+            else -> {
+                binding.hourColorChanger.setBackgroundColor(color)
+                binding.vkTestClocks.hourHandColor = color
+            }
+        }
+    }
+
+    override fun onDialogDismissed(dialogId: Int) {
+    }
+
+    companion object{
+        const val SECOND_COLOR_DIALOG_ID = 0
+        const val MINUTE_COLOR_DIALOG_ID = 1
+        const val HOUR_COLOR_DIALOG_ID = 2
     }
 }
